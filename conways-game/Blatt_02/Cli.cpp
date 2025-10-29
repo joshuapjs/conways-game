8722e8fd15f9b8281e541b2deafc9a7144a3c430
Joshua Spingler <82243579+joshuapjs@users.noreply.github.com>
2025-10-29 20:06:42 +0100

    Small adjustment on first Cli draft + .gitignore extended

--- conways-game/Blatt_02/Cli.cpp
+++ conways-game/Blatt_02/Cli.cpp
@@ -2,13 +2,14 @@
 # include <string>
 # include <iostream>
 
+// The implementation of Cli::start
 void Cli::start() {
     std::string input;
     bool q = false;
     while (!q) {
         std::cout << "CGOL>";
         getline(std::cin, input);
-        if (!input.compare("quit")) {
+        if (input == "quit") {
             q = true;
             break;
         };
