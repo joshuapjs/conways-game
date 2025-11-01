eab515fb82a0174a44398f6cf9c2a2d5a8500b1b
Tim Wolf <s9677570@stud.uni-frankfurt.de>
2025-11-01 21:09:39 +0100

    Add World

--- conways-game/Blatt_02/main.cpp
+++ conways-game/Blatt_02/main.cpp
@@ -1,7 +1,33 @@
-# include <iostream>
-# include "Cli.h"
+#include <iostream>
+#include "World.h"
 
-int main(int argc, char* argv[]) { 
-    Cli cli = Cli();
-    cli.start();
+int main() {
+    World world(5, 5);
+
+    if (!world.load("p67_snark_loop.txt")) {
+        std::cerr << "Failed to load from file.\n";
+        return 1;
+    }
+
+    world.print();
+    std::cout << '\n';
+
+    for (int i = 0; i < 100; ++i) {
+        world.evolve();
+        std::cout << "Generation " << i + 1 << ":\n";
+        //game.print();
+        std::cout << '\n';
+
+        if (world.isStable()) {
+            std::cout << "Stabilized at generation " << i + 1 << ".\n";
+            break;
+        }
+    }
+
+    if (!world.save("output.txt")) {
+        std::cerr << "Failed to save to file.\n";
+        return 1;
+    }
+
+    return 0;
 }
