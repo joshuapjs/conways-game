1f1c93e6e79ba8ad9794d6b229e2bff532767b6f
luiz-feitosa <feitosa@protonmail.com>
2025-11-04 21:28:00 +0100

    fix Cli.h

--- conways-game/Blatt_02/Cli.h
+++ conways-game/Blatt_02/Cli.h
@@ -1,6 +1,8 @@
 #ifndef CLI
 #define CLI
+
 #include <memory>
+#include <string>
 
 class World;
 
@@ -10,29 +12,30 @@ private:
     int delayTime;           //delay in milliseconds
     bool stabilityCheck;     
     std::unique_ptr<World> world; 
-    void save(std::string& input);
-    void load(std::string& input);
+
     void create(std::string& input);
-    
-public:
-    Cli();
-    ~Cli();
-    void start();
-    void addBeacon(std::string& input);
-    void addToad(std::string& input);
-    void addGlider(std::string& input);
-    void getOneD(std::string& input);
-    void getTwoD(std::string& input);
-    void setOneD(std::string& input);
-    void setTwoD(std::string& input);
-    void getOneD(std::string& input);
-    void setOneD(std::string& input);
-    void setTwoD(std::string& input);
+    void load(std::string& input);
+    void save(std::string& input);
+
     void run(std::string& input);
     void stability(std::string& input);
     void delay(std::string& input);
     void print(std::string& input);
     void help(std::string& input);
+
+    void addBeacon(std::string& input);
+    void addToad(std::string& input);
+    void addGlider(std::string& input);
+
+    void getOneD(std::string& input);
+    void getTwoD(std::string& input);
+    void setOneD(std::string& input);
+    void setTwoD(std::string& input);
+
+public:
+    Cli();
+    ~Cli();
+    void start();
 };
 
 #endif
